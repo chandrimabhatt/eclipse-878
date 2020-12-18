cd769d20c8620a6dd23400a822167f6a354bcbe3
Chandrima Bhattacharjee <chandrimabhattacharjee07@gmail.com>
2020-12-18 10:22:52 +0530

    commit in average.java

--- Demo_new/src/Demo11/Average.java
+++ Demo_new/src/Demo11/Average.java
@@ -4,7 +4,7 @@ public class Average {
 	
         
         public static void main(String args[]){  
-        	int number=13;    
+        	int number=15;    
             //Using ternary operator  
             String put=(number%2==0)?"even number":"odd number";
             System.out.println(put);   
